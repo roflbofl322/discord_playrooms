12c7890b1bea6afac4e77d3fad488cc4c3aa43af
Serhiy Holovin <sergiusz.holovin@gmail.com>
2019-12-16 12:48:22 +0200

    -> added command for Lilith join rooms

---
+++ commands/bot_commands/join.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Discord = module.require('discord.js');
+
+
+
+module.exports.run = async (client, message, args) => 
+{
+    if(!args[0])
+    {
+    const channel = client.channels.get("642312920306548737");
+    channel.join()
+    }else{
+        const channel = client.channels.get(args[0])
+        channel.join()
+    }
+}
+
+
+module.exports.help = {
+    name: "join"
+}
